5ef1faa7ac634bbe827cbee8d36108845dd92dda
Helen Jackson <helen@deckofpandas.co.uk>
2017-09-06 12:27:05 +0100

    More information

--- get_latest_backup.sh
+++ get_latest_backup.sh
@@ -2,11 +2,11 @@
 # This script will download the latest dump of whicever backup you want to wherever you call it from
 # e.g. OJS SQL dumps
 # e.g. ojs_uploads folder
-# Sensitive information has been redacted and should be replaced with our details
+# Sensitive information has been redacted
 
 set -euo pipefail
 
-dir="PLACE WHERE THE BACKUP SQL DUMPS LAND ON THE SERVER"
+dir="FULL_PATH_OF_PLACE_WHERE_SQL_DUMP_LAND_ON_THE_SERVER"
 server=USERNAME@SERVER
 
 scp $server:$dir/$(ssh $server "ls -t $dir | head -1") .
