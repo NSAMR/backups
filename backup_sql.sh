095e203299b21f72da0ef253a4b02e9cf486a7c3
Helen Jackson <helen@deckofpandas.co.uk>
2017-09-06 12:24:26 +0100

    Generalising and comments

--- backup_sql.sh
+++ backup_sql.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# This script will drop an SQL dump of the OJS database in the specified folder
+# Sensitive details have been redacted
 set -euo pipefail
 
-mysqldump --host mysql-55.int.mythic-beasts.com -u nsamr -pPASSWORD_GOES_HERE nsamr | gzip > /home/nsamr/backups/BACKUP.$(date +"%Y-%m-%d-%H-%M").nsamr.bak.sql.gz
+mysqldump --host SQLSERVERADDRESS -u USERNAME -pPASSWORD OJS_DBNAME | gzip > /home/nsamr/backups/BACKUP.$(date +"%Y-%m-%d-%H-%M").nsamr.bak.sql.gz
