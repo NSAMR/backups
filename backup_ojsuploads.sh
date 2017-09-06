2929729c0e9ef67749da1bf6961acf63143fecd8
Helen Jackson <helen@deckofpandas.co.uk>
2017-09-06 12:25:29 +0100

    Comments and redaction

--- backup_ojsuploads.sh
+++ backup_ojsuploads.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# This script will drop .tgz of the ojs_uploads in the specified folder
+# Sensitive details have been redacted
+
 set -euo pipefail
 
-tar -zcvf BACKUP_OJSUPLOADS.$(date +"%Y-%m-%d-%H-%M").nsamr.bak.sql.tgz /home/nsamr/www/ojs_uploads/journals
+tar -zcvf BACKUP_OJSUPLOADS.$(date +"%Y-%m-%d-%H-%M").nsamr.bak.sql.tgz FULL_PATH_TO_OJS_UPLOADS_FOLDER
